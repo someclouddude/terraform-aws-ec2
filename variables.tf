6f8ee364bae9bc16492f5b15f2f8a5c64a95fd31
someclouddude <42011691+someclouddude@users.noreply.github.com>
2019-01-15 21:43:21 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,11 @@
 variable "ssh_key" {
   description = "the ssh key to use for access"
 }
+
+variable "aws_key" {
+  description = "aws access key"
+  }
+
+variable "aws_secret" {
+  description = "aws secret key"
+  }
